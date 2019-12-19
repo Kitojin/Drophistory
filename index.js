135c17ccc62457841e90daae1a4b24226881abaf
Nekonekoo <patrickfetscher60@gmail.com>
2019-12-19 23:53:41 +0100

    Big Patch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const items = require('./items');
 const config = require('./config');
-const rarityColours = ['#dddddd','#22ff00','#00ddff','#ffbb00']
+const rarityColours = ['#ffffff','#22ff00','#00ddff','#ffcc00']
 
 module.exports = function drophistory(mod){
 	let enabled = config.toggle;
@@ -9,22 +9,33 @@ module.exports = function drophistory(mod){
 		enabled =! enabled;
 		mod.command.message(`drophistory ${enabled ? 'en' : 'dis'}abled`);
 	});
-	
+	/*
 	mod.hook('S_SPAWN_DROPITEM', 8, e=>{
 		if (!enabled) return;
 		var res = items.find(item => item.id == e.item);
 		//TODO: Query datacenter to get localised strings?
 		if(res){mod.command.message(`Dropped <font color="${rarityColours[res.rarity]}">${res.name_string}</font>`);}
-	});
+	});*/
 	
-	mod.hook('S_SYSTEM_MESSAGE',1,e=>{
-		if(enabled&&e.message.startsWith('@679')){
-			var msg = e.message.split('\u000b');
-			var res = items.find(item => item.id == msg[4].slice(6));
-			if(res){
-				mod.command.message(`${msg[2]} dropped <font color="${rarityColours[res.rarity]}">${msg[6]}x ${res.name_string}</font>`)
-			};
+	mod.hook('S_SYSTEM_MESSAGE', 1,e=>{
+		if (!enabled) return;
+		const msg = mod.parseSystemMessage(e.message);
+		if(msg.id=='SMT_PARTY_LOOT_ITEM_PARTYPLAYER'){
+			//split up item token
+			msg.tokens.ItemName = msg.tokens.ItemName.replace('@item:','').split('?dbid');
+			
+			//filter by whitelist
+			if (!items.whitelist.includes(parseInt(msg.tokens.ItemName[0]))) return;
+			var rarity;
+			mod.queryData('/ItemData/Item@id=?/',[parseInt(msg.tokens.ItemName[0])]).then(res =>{
+				rarity = res.attributes.rareGrade;
+			}).catch(e=>{console.log(e);});
+			
+			//add admount of stats to exo items.
+			mod.queryData('/StrSheet_Item/String@id=?',[parseInt(msg.tokens.ItemName[0])]).then(res=>{
+				statItem = items.statItems.find(i=>i.id==msg.tokens.ItemName[0]);
+				mod.command.message(`${msg.tokens.PartyPlayerName} picked up <font color="${rarityColours[rarity]}">${msg.tokens.ItemAmount}x ${res.attributes.string + (statItem ? statItem.statString : '')}</font>`)
+			}).catch(e=>{console.log(e);});
 		}
 	});
-	
 }
