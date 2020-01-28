fc18d39b9b78f353cc623b6200c84cc4d971a18f
Phr3nic <47946595+Phr3nic@users.noreply.github.com>
2020-01-28 21:00:33 +0100

    Finalised inital build to push to master
    For a summary of all the current functionality, refer to the readme.

--- index.js
+++ index.js
@@ -1,82 +1,90 @@
-const whitelist = require('./whitelist');
-const exoItems = require('./exoItems');
-const config = require('./config');
-const rarityColours = ['#ffffff','#22ff00','#00ccff','#ffcc00']
 const fs = require('fs');
 
-module.exports = function drophistory(mod){
-	let enabled = config.toggle;
-	let partyLogging = config.partyLogging;
-	let selfLogging = config.selfLoggingg;
-	let logToFile = config.logToFile;
-	let logToChat = config.logToChat;
-
-	let currentParty = [];
+module.exports = function drophistory(mod) {
+	let config = mod.settings;
 
 	const players = new Set();
 	const itemNames = new Map();
-	for(let item of whitelist){
-		res = mod.queryData('/StrSheet_Item/String@id=?',[parseInt(item)]).then(res=>{
-			itemNames.set(item, res.attributes.string);
-		})
-		.catch(e=>{mod.error(e)});
-	}
+
+	let currentParty = [];
 	
-	mod.command.add('dh',{
-		$none(){
-			enabled =! enabled;
-			mod.command.message(`drophistory ${enabled ? 'en' : 'dis'}abled`);
+	for (let item of config.whitelist) {
+		mod.queryData('/StrSheet_Item/String@id=?', [parseInt(item)]).then(res => {
+			itemNames.set(+item, res.attributes.string);
+		}).catch(e => { mod.error(e) });
+	}
+
+	mod.command.add('dh', {
+		$none() {
+			config.enable = !config.enable;
+			mod.command.message(`drophistory ${config.enable ? 'en' : 'dis'}abled`);
 		},
 
-		log(...args){
-			if(args.length==0){
+		log(...args) {
+			if (args.length == 0) {
 				mod.command.message('Usage: dh log [file|chat|party|self] to toggle modes')
+				return;
 			} else {
-				if(args.includes('file')){	
-					logToFile = !logToFile;
-					if(!logToFile)saveLog();
+				if (args.includes('file')) {
+					config.logToFile = !config.logToFile;
+					if (!config.logToFile) saveLog();
 				}
-				if(args.includes('chat')) 	logToChat = !logToChat;
-				if(args.includes('party')){ 	
-					partyLogging = !partyLogging;
-					if(partyLogging) addPartyMembers();
+				if (args.includes('chat')) {
+					config.logToChat = !config.logToChat;
 				}
-				if(args.includes('self')){	
-					selfLogging = !selfLogging;
-					if(selfLogging) AddToSetIfNew(mod.game.me.name);
+				if (args.includes('party')) {
+					config.partyLogging = !config.partyLogging;
+					if (config.partyLogging) addPartyMembers();
+				}
+				if (args.includes('self')) {
+					config.selfLogging = !config.selfLogging;
+					if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
 				}
 			}
 			showCurrentLogState();
 		},
 
-		save(){
+		save() {
 			saveLog()
+			//Saving the log clears players that are being kept track of, so readd them here
+			if (config.partyLogging) addPartyMembers();
+			if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
 		},
 
-		whitelist(item){
-			//TODO: strip chatlink to get item id
-			console.log(item);
-			//TODO: Add item to whitelist and re-query DC to get localised itemname
-			/*whitelist.push(Number(item))
-			mod.queryData('/StrSheet_Item/String@id=?',[parseInt(item)]).then(res=>{
-				itemNames.set(item, res.attributes.string);
-			}).catch(e=>{mod.error(e)});*/
+		whitelist:{
+			add(item){
+				//strip chatlink to get item id
+				item = item.replace(/^<.*#+/, '').replace(/@.+$/,'').trim();
+				//Add item to whitelist and re-query DC to get localised itemname
+				config.whitelist.push(parseInt(item));
+				mod.queryData('/StrSheet_Item/String@id=?',[parseInt(item)]).then(res=>{
+					itemNames.set(+item, res.attributes.string);
+					mod.command.message(`added ${res.attributes.string}`)
+				}).catch(e=>{mod.error(e)});	
+				
+			},
+			remove(item){
+				item = item.replace(/^<.*#+/, '').replace(/@.+$/,'').trim();
+				config.whitelist = config.whitelist.filter(itm => itm != item);
+				mod.command.message(`removed ${itemNames.get(item)}`);
+				itemNames.delete(item);
+			}
 		},
 
-		$default(){
+		$default() {
 			mod.command.message('Usage: dh [log|whitelist|save]')
 		}
 	});
 
-	function isInSet(name){
-		for(let player of players){
-			if(player.name == name) return true;
+	function isInSet(name) {
+		for (let player of players) {
+			if (player.name == name) return true;
 		}
 		return false;
 	}
 
-	function AddToSetIfNew(name){
-		if(!isInSet(name)){
+	function AddToSetIfNew(name) {
+		if (!isInSet(name)) {
 			let newPlayer = {
 				name: name,
 				droppedItems: new Map()
@@ -85,109 +93,154 @@ module.exports = function drophistory(mod){
 		}
 	}
 
-	function addPartyMembers(){
-		for(let player of currentParty){
+	function addPartyMembers() {
+		for (let player of currentParty) {
 			AddToSetIfNew(player.name)
 		}
 	}
 
-	function showCurrentLogState(){
-		let logFrom = `${selfLogging && partyLogging ? 'self and party' : selfLogging ? 'self' : partyLogging ? 'party' : 'nobody'}`
-		let logTo = `${logToChat&&logToFile ? 'in chat and file.' : logToChat ? 'in chat.' : logToFile ? 'in file.' : 'nowhere!'}`
+	function showCurrentLogState() {
+		let logFrom = '';
+		if (config.selfLogging && config.partyLogging) {
+			logFrom = 'self and party';
+		} else if (config.selfLogging) {
+			logFrom = 'self';
+		} else if (config.partyLogging) {
+			logFrom = 'party';
+		} else {
+			logFrom = 'nobody'
+		}
+
+		let logTo = '';
+		if (config.logToChat && config.logToFile) {
+			logTo = 'in chat and file'
+		} else if (config.logToChat) {
+			logTo = 'in chat'
+		} else if (config.logToFile) {
+			logTo = 'in file'
+		} else {
+			logTo = 'nowhere!'
+		}
+
 		mod.command.message(`Currently logging for: ${logFrom}, log is shown ${logTo}`);
 	}
 
-	function normaliseItemToken(msg, e){
+	function normaliseItemToken(msg, e) {
 		//Split up itemname into id and dbid (if existant) and streamline field names
-		msg.tokens.ItemName = msg.tokens.ItemName.replace('@item:','').split('?dbid:');
-		if(msg.id=='SMT_LOOTED_SPECIAL_ITEM'){msg.tokens.ItemName[1]=''+e.uniqueId;}
+		msg.tokens.ItemName = msg.tokens.ItemName.replace('@item:', '').split('?dbid:');
+		if (msg.id == 'SMT_LOOTED_SPECIAL_ITEM') { msg.tokens.ItemName[1] = '' + e.uniqueId; }
 		msg.tokens.UserName = ('UserName' in msg.tokens) ? msg.tokens.UserName : msg.tokens.PartyPlayerName;
 	}
-	
-	async function printChatLog(msg){
+
+	async function printChatLog(msg) {
 		//grab item rarity via DC query
-		const rarityRes = await mod.queryData('/ItemData/Item@id=?/',[parseInt(msg.tokens.ItemName[0])]).catch(e=>{mod.error(e)});
+		const rarityRes = await mod.queryData('/ItemData/Item@id=?/', [parseInt(msg.tokens.ItemName[0])]).catch(e => { mod.error(e) });
 		const rarity = rarityRes.attributes.rareGrade;
-		
+
 		//add admount of stats to exo items and construct chat message.
-		const statItem = exoItems.find(i=>i.id==msg.tokens.ItemName[0]);
+		const statItem = config.exoItems.find(i => i.id == msg.tokens.ItemName[0]);
 		const chatActionLinkString = `<ChatLinkAction param=\"1#####${msg.tokens.ItemName[0]}@${msg.tokens.ItemName[1]}@${msg.tokens.UserName}\">`;
 		//This is an abomination, don't look at it. Seriously. DO NOT TOUCH!
-		mod.command.message(`${msg.tokens.UserName} picked up <font color="${rarityColours[rarity]}">${(msg.tokens.ItemName.length > 1 ? chatActionLinkString : '')}&lt;${msg.tokens.ItemAmount}x ${itemNames.get(parseInt(msg.tokens.ItemName[0])) + (statItem ? statItem.statString : '')}&gt;${(msg.tokens.ItemName.length > 1 ? '</ChatLinkAction>' : '')}</font>`);
+
+		let chatMsg = [
+			`${msg.tokens.UserName} picked up`,																//playername
+			`<font color="${config.rarityColours[rarity]}">`, 												//font colour
+			`${(msg.tokens.ItemName.length > 1 ? chatActionLinkString : '')}`, 								//if item is a gearpiece (has dbid), link it
+			`&lt;${msg.tokens.ItemAmount}x `,																//amount
+			`${itemNames.get(parseInt(msg.tokens.ItemName[0])) + (statItem ? statItem.statString : '')}`,	//get localised String. if it's exo gear, append amount of stats
+			`&gt;${(msg.tokens.ItemName.length > 1 ? '</ChatLinkAction>' : '')}</font>`						//close link tag if item is a gearpiece
+		]
+		mod.command.message(chatMsg.join(''))
+		//mod.command.message(`${msg.tokens.UserName} picked up <font color="${rarityColours[rarity]}">${(msg.tokens.ItemName.length > 1 ? chatActionLinkString : '')}&lt;${msg.tokens.ItemAmount}x ${itemNames.get(parseInt(msg.tokens.ItemName[0])) + (statItem ? statItem.statString : '')}&gt;${(msg.tokens.ItemName.length > 1 ? '</ChatLinkAction>' : '')}</font>`);
 	}
 
-	function updateFileLog(msg){
-		if(!isInSet(msg.tokens.UserName)) mod.error(`[DEBUG]:${msg.tokens.UserName} not in Set!`)
-		for (const player of players){
-			if(player.name == msg.tokens.UserName){
-				//if item already in Map, add up amounts
-				if(player.droppedItems.has(itemNames.get(parseInt(msg.tokens.ItemName[0])))){
-					player.droppedItems.set(
-						itemNames.get(parseInt(msg.tokens.ItemName[0])), //get Itemname from ID in message...
-						player.droppedItems.get(itemNames.get(parseInt(msg.tokens.ItemName[0])))+parseInt(msg.tokens.ItemAmount) //
-					);
-				} else { //otherwise add new item
-					player.droppedItems.set(
-						itemNames.get(parseInt(msg.tokens.ItemName[0])), 
-						parseInt(msg.tokens.ItemAmount)
-					);
-				}
+	function updateFileLog(msg) {
+		if (!isInSet(msg.tokens.UserName)) mod.error(`[DEBUG]:${msg.tokens.UserName} not in Set!`)
+
+		for (const player of players) {
+			if (player.name != msg.tokens.UserName) continue;
+			//if item already in Map, add up amounts
+			if (player.droppedItems.has(itemNames.get(parseInt(msg.tokens.ItemName[0])))) {
+				player.droppedItems.set(
+					itemNames.get(parseInt(msg.tokens.ItemName[0])), //get Itemname from ID in message...
+					player.droppedItems.get(itemNames.get(parseInt(msg.tokens.ItemName[0]))) + parseInt(msg.tokens.ItemAmount) //
+				);
+			} else { //otherwise add new item
+				player.droppedItems.set(
+					itemNames.get(parseInt(msg.tokens.ItemName[0])),
+					parseInt(msg.tokens.ItemAmount)
+				);
 			}
+
 		}
 	}
 
-	function handleSysMsg(e){
-		if(!enabled) return
-		if(!logToChat&&!logToFile) return
+	function handleSysMsg(e) {
+		if (!config.enable) return
+		if (!config.logToChat && !config.logToFile) return
 
 		var msg = mod.parseSystemMessage(e.message ? e.message : e.sysmsg);
-		if(msg.id=='SMT_PARTY_LOOT_ITEM_PARTYPLAYER'||msg.id=='SMT_LOOTED_ITEM'||msg.id=='SMT_LOOTED_SPECIAL_ITEM'){
+		if (msg.id == 'SMT_PARTY_LOOT_ITEM_PARTYPLAYER' || msg.id == 'SMT_LOOTED_ITEM' || msg.id == 'SMT_LOOTED_SPECIAL_ITEM') {
 			normaliseItemToken(msg, e);
-			
-			//filter by whitelist and logging mode
-			if (!whitelist.includes(parseInt(msg.tokens.ItemName[0])))return;
-			if (!partyLogging 	&& msg.tokens.UserName != mod.game.me.name) return;
-			if (!selfLogging 	&& msg.tokens.UserName == mod.game.me.name) return;
 
-			if(logToChat) printChatLog(msg);
-			if(logToFile) updateFileLog(msg);
-		}	
+			//filter by whitelist and logging mode
+			if (!config.whitelist.includes(parseInt(msg.tokens.ItemName[0]))) return;
+			if (!config.partyLogging && msg.tokens.UserName != mod.game.me.name) return;
+			if (!config.selfLogging && msg.tokens.UserName == mod.game.me.name) return;
+
+			if (config.logToChat) printChatLog(msg);
+			if (config.logToFile) updateFileLog(msg);
+		}
 	}
 
-	function saveLog(){
-		let logOutput = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-		for(const player of players){
-			if(player.droppedItems.size>0){
-				logOutput+='\t'+player.name
-				for(let item of player.droppedItems){
-					logOutput+='\t\t> '+item[1]+'x '+item[0]+'\r\n'
-				}
+	function saveLog() {
+		if(players.size==0) return;
+
+		let logIsEmpty = true;
+		let logOutput = '';
+		
+		for (const player of players) {
+			if (player.droppedItems.size == 0) continue;
+			logIsEmpty = false;
+			logOutput += '\n\t' + player.name
+			for (let item of player.droppedItems) {
+				logOutput += '\n\t\t> ' + item[1] + 'x ' + item[0]
 			}
-			logOutput+='\n\r'
+			logOutput += '\n'
 		}
+
+		if(!logIsEmpty){
+			logOutput = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + logOutput;
+		}
+
 		players.clear();
-		fs.appendFile('DropLog.txt',logOutput,(err)=>{
-			if(err){ throw (err);} else mod.log('Log saved!');
+		logIsEmpty = true;
+
+		fs.appendFile('DropLog.txt', logOutput, (err) => {
+			if (err) {
+				mod.error(err);
+			} else {
+				mod.log('Log saved!');
+			}
 		});
 	}
 
 	mod.hook('S_SYSTEM_MESSAGE', 1, handleSysMsg);
 	mod.hook('S_SYSTEM_MESSAGE_LOOT_SPECIAL_ITEM', 1, handleSysMsg);
-	mod.hook('S_SYSTEM_MESSAGE_LOOT_ITEM',1, handleSysMsg);
+	mod.hook('S_SYSTEM_MESSAGE_LOOT_ITEM', 1, handleSysMsg);
 
-	mod.hook('S_PARTY_MEMBER_LIST',7,e=>{
-		if (!enabled) return;
+	mod.hook('S_PARTY_MEMBER_LIST', 7, e => {
+		if (!config.enable) return;
 		currentParty = e.members;
-		if(!partyLogging) return;
+		if (!config.partyLogging) return;
 		addPartyMembers();
 	});
 
-	mod.hook('S_LOGIN',14,e=>{
-		if(selfLogging) AddToSetIfNew(mod.game.me.name);
+	mod.hook('S_LOGIN', 14, e => {
+		if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
 	})
 
-	this.destructor = ()=>{
-		if(logToFile) saveLog();
-		mod.log('Destructor got called!')
+	this.destructor = () => {
+		if (config.logToFile) saveLog();
 	}
 }
--- readme.md
+++ readme.md
@@ -1,31 +1,38 @@
-## Info:
+# Drophistory
 
-- Sends a message in proxy chat when selected items drop.
-- Item names are directly queried from Datacenter and as such are localised to your client's language.
+* Can keep track of items that are picked up
+* Item names are taken directly from datacenter and as such localised to your client's language
+* Fully customisable whitelist to not clutter your chat/log with unneccessary items
+* Customisable to only keep track of you, your party, or both
 
-
+* Customisable to either directly notify you in proxy chat or save the log to a file
+  * Colour codes by item rarity in chat
+  * Adds amount of stats (0 - 3) to the item names of new gear
+  * Gear Pieces are linked to chat, clicking them will show their tooltip
 ---
 
 ## Commands:
 
-- **drophistory**
-  - to enable/disable this Mod.
+* **`dh`**
+  * to enable/disable this Mod.
+  * **`file/chat/party/self`**
+    * to toggle between different logging modes
+	* multiple modes can be toggle at the same time (e.g. `dh file chat self` to toggle all three at once)
+  * **`save`**
+    * to save the current log to a file (empty if file logging is disabled)
+  * **`whitelist`**
+    * **`add/remove`**
+	  * add or remove items from your whitelist. Simply link the item in chat or type in the item id.
 
 ---
 
-## Notes:
+### Possible future features:
 
-Supported Items:
+<details>
 
-- Diamonds
-- Left/Right Part of Treasure Map
+* adding blacklist support
+* toggling individual players to be tracked instead of the whole party
 
-- All types of Transcendent/Bahaar Masks
-- Exodor Tokens
-- Exodor Gearpieces
-- Relics & Haildoms
+</details>
 
-## Roadmap:
-- better grp member overview coming soon
-- and more 
-
+Original idea by [PatrickSantoZZ](https://github.com/PatrickSantoZZ)
