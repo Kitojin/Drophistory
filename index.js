07c42bf808fa9d5c0248c3e06e21810c98cc33f5
Phr3nic <47946595+Phr3nic@users.noreply.github.com>
2020-09-11 14:20:35 +0200

    Removed DC queries
    > Instead of taxing the client with DC queries, using the pre-cached data provided by tera-game-state
    > slightly reworked config code for cleaner code

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ module.exports = function drophistory(mod) {
 	const itemNames = new Map();
 
 	let currentParty = [];
-	
-	for (let item of config.whitelist) {
-		mod.queryData('/StrSheet_Item/String@id=?', [parseInt(item)]).then(res => {
-			itemNames.set(+item, res.attributes.string);
-		}).catch(e => { mod.error(e) });
-	}
+
+	config.whitelist.forEach(item => {
+		itemNames.set(+item, mod.game.data.items.get(item).name);
+	});
 
 	mod.command.add('dh', {
 		$none() {
@@ -21,64 +19,85 @@ module.exports = function drophistory(mod) {
 		},
 
 		log(...args) {
-			if (args.length == 0) {
-				mod.command.message('Usage: dh log [file|chat|party|self] to toggle modes')
+			if (args.length === 0) {
+				mod.command.message(
+					'Usage: dh log [file|chat|party|self] to toggle modes',
+				);
 				return;
-			} else {
-				if (args.includes('file')) {
-					config.logToFile = !config.logToFile;
-					if (!config.logToFile) saveLog();
-				}
-				if (args.includes('chat')) {
-					config.logToChat = !config.logToChat;
-				}
-				if (args.includes('party')) {
-					config.partyLogging = !config.partyLogging;
-					if (config.partyLogging) addPartyMembers();
-				}
-				if (args.includes('self')) {
-					config.selfLogging = !config.selfLogging;
-					if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
-				}
 			}
+
+			args.foreach(arg => {
+				arg = arg.toLowerCase();
+				if (config.logMode[arg]) config.logMode[arg] = !config.logMode[arg];
+			});
+
+			// config.logToFile = args.includes('file')
+			// 	? !config.logToFile
+			// 	: config.logToFile;
+
+			// config.logToChat = args.includes('chat')
+			// 	? !config.logToChat
+			// 	: config.logToChat;
+
+			// config.partyLogging = args.includes('party')
+			// 	? !config.partyLogging
+			// 	: config.partyLogging;
+
+			// config.selfLogging = args.includes('self')
+			// 	? !config.selfLogging
+			// 	: config.selfLogging;
+
+			// if (config.partyLogging) addPartyMembers();
+			// if (!config.logToFile) saveLog();
+			// if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
+			if (config.logMode.party) addPartyMembers();
+			if (!config.logMode.file) saveLog();
+			if (config.logMode.self) AddToSetIfNew(mod.game.me.name);
+
 			showCurrentLogState();
 		},
 
 		save() {
-			saveLog()
+			saveLog();
 			//Saving the log clears players that are being kept track of, so readd them here
-			if (config.partyLogging) addPartyMembers();
-			if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
+			// if (config.partyLogging) addPartyMembers();
+			// if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
+			if (config.logMode.party) addPartyMembers();
+			if (config.logMode.self) AddToSetIfNew(mod.game.me.name);
 		},
 
-		whitelist:{
-			add(item){
+		whitelist: {
+			add(item) {
 				//strip chatlink to get item id
-				item = item.replace(/^<.*#+/, '').replace(/@.+$/,'').trim();
-				//Add item to whitelist and re-query DC to get localised itemname
+				item = item
+					.replace(/^<.*#+/, '')
+					.replace(/@.+$/, '')
+					.trim();
+				//Add item to whitelist and list of itemnames
 				config.whitelist.push(parseInt(item));
-				mod.queryData('/StrSheet_Item/String@id=?',[parseInt(item)]).then(res=>{
-					itemNames.set(+item, res.attributes.string);
-					mod.command.message(`added ${res.attributes.string}`)
-				}).catch(e=>{mod.error(e)});	
-				
+
+				itemNames.set(+item, mod.game.data.items.get(item).name);
+				mod.command.message(`added ${mod.game.data.items.get(item).name}`);
 			},
-			remove(item){
-				item = item.replace(/^<.*#+/, '').replace(/@.+$/,'').trim();
+			remove(item) {
+				item = item
+					.replace(/^<.*#+/, '')
+					.replace(/@.+$/, '')
+					.trim();
 				config.whitelist = config.whitelist.filter(itm => itm != item);
 				mod.command.message(`removed ${itemNames.get(item)}`);
 				itemNames.delete(item);
-			}
+			},
 		},
 
 		$default() {
-			mod.command.message('Usage: dh [log|whitelist|save]')
-		}
+			mod.command.message('Usage: dh [log|whitelist|save]');
+		},
 	});
 
 	function isInSet(name) {
 		for (let player of players) {
-			if (player.name == name) return true;
+			if (player.name === name) return true;
 		}
 		return false;
 	}
@@ -87,134 +106,174 @@ module.exports = function drophistory(mod) {
 		if (!isInSet(name)) {
 			let newPlayer = {
 				name: name,
-				droppedItems: new Map()
-			}
+				droppedItems: new Map(),
+			};
 			players.add(newPlayer);
 		}
 	}
 
 	function addPartyMembers() {
 		for (let player of currentParty) {
-			AddToSetIfNew(player.name)
+			AddToSetIfNew(player.name);
 		}
 	}
 
 	function showCurrentLogState() {
 		let logFrom = '';
-		if (config.selfLogging && config.partyLogging) {
+		// if (config.selfLogging && config.partyLogging) {
+		// 	logFrom = 'self and party';
+		// } else if (config.selfLogging) {
+		// 	logFrom = 'self';
+		// } else if (config.partyLogging) {
+		// 	logFrom = 'party';
+		// } else {
+		// 	logFrom = 'nobody';
+		// }
+		if (config.logMode.self && config.logMode.party) {
 			logFrom = 'self and party';
-		} else if (config.selfLogging) {
+		} else if (config.logMode.self) {
 			logFrom = 'self';
-		} else if (config.partyLogging) {
+		} else if (config.logMode.party) {
 			logFrom = 'party';
 		} else {
-			logFrom = 'nobody'
+			logFrom = 'nobody';
 		}
 
 		let logTo = '';
-		if (config.logToChat && config.logToFile) {
-			logTo = 'in chat and file'
-		} else if (config.logToChat) {
-			logTo = 'in chat'
-		} else if (config.logToFile) {
-			logTo = 'in file'
+		// if (config.logToChat && config.logToFile) {
+		// 	logTo = 'in chat and file';
+		// } else if (config.logToChat) {
+		// 	logTo = 'in chat';
+		// } else if (config.logToFile) {
+		// 	logTo = 'in file';
+		// } else {
+		// 	logTo = 'nowhere!';
+		// }
+		if (config.logMode.chat && config.logMode.file) {
+			logTo = 'in chat and file';
+		} else if (config.logMode.chat) {
+			logTo = 'in chat';
+		} else if (config.logMode.file) {
+			logTo = 'in file';
 		} else {
-			logTo = 'nowhere!'
+			logTo = 'nowhere!';
 		}
 
-		mod.command.message(`Currently logging for: ${logFrom}, log is shown ${logTo}`);
+		mod.command.message(
+			`Currently logging for: ${logFrom}, log is shown ${logTo}`,
+		);
 	}
 
 	function normaliseItemToken(msg, e) {
 		//Split up itemname into id and dbid (if existant) and streamline field names
-		msg.tokens.ItemName = msg.tokens.ItemName.replace('@item:', '').split('?dbid:');
-		if (msg.id == 'SMT_LOOTED_SPECIAL_ITEM') { msg.tokens.ItemName[1] = '' + e.uniqueId; }
-		msg.tokens.UserName = ('UserName' in msg.tokens) ? msg.tokens.UserName : msg.tokens.PartyPlayerName;
+		msg.tokens.ItemName = msg.tokens.ItemName.replace('@item:', '').split(
+			'?dbid:',
+		);
+		if (msg.id == 'SMT_LOOTED_SPECIAL_ITEM') {
+			msg.tokens.ItemName[1] = '' + e.uniqueId;
+		}
+		msg.tokens.UserName =
+			'UserName' in msg.tokens
+				? msg.tokens.UserName
+				: msg.tokens.PartyPlayerName;
 	}
 
 	async function printChatLog(msg) {
-		//grab item rarity via DC query
-		const rarityRes = await mod.queryData('/ItemData/Item@id=?/', [parseInt(msg.tokens.ItemName[0])]).catch(e => { mod.error(e) });
-		const rarity = rarityRes.attributes.rareGrade;
+		const rarity = mod.game.data.items.get(msg.tokens.itemName[0]).rareGrade;
 
 		//add admount of stats to exo items and construct chat message.
 		const statItem = config.exoItems.find(i => i.id == msg.tokens.ItemName[0]);
 		const chatActionLinkString = `<ChatLinkAction param=\"1#####${msg.tokens.ItemName[0]}@${msg.tokens.ItemName[1]}@${msg.tokens.UserName}\">`;
 
 		let chatMsg = [
-			`${msg.tokens.UserName} picked up`,																//playername
-			`<font color="${config.rarityColours[rarity]}">`, 												//font colour
-			`${(msg.tokens.ItemName.length > 1 ? chatActionLinkString : '')}`, 								//if item is a gearpiece (has dbid), link it
-			`&lt;${msg.tokens.ItemAmount}x `,																//amount
-			`${itemNames.get(parseInt(msg.tokens.ItemName[0])) + (statItem ? statItem.statString : '')}`,	//get localised String. if it's exo gear, append amount of stats
-			`&gt;${(msg.tokens.ItemName.length > 1 ? '</ChatLinkAction>' : '')}</font>`						//close link tag if item is a gearpiece
-		]
-		mod.command.message(chatMsg.join(''))
+			`${msg.tokens.UserName} picked up`, //playername
+			`<font color="${config.rarityColours[rarity]}">`, //font colour
+			`${msg.tokens.ItemName.length > 1 ? chatActionLinkString : ''}`, //if item is a gearpiece (has dbid), link it
+			`&lt;${msg.tokens.ItemAmount}x `, //amount
+			`${
+				itemNames.get(parseInt(msg.tokens.ItemName[0])) +
+				(statItem ? statItem.statString : '')
+			}`, //get localised String. if it's exo gear, append amount of stats
+			`&gt;${msg.tokens.ItemName.length > 1 ? '</ChatLinkAction>' : ''}</font>`, //close link tag if item is a gearpiece
+		];
+		mod.command.message(chatMsg.join(''));
 	}
 
 	function updateFileLog(msg) {
-		if (!isInSet(msg.tokens.UserName)) mod.error(`[DEBUG]:${msg.tokens.UserName} not in Set!`)
+		if (!isInSet(msg.tokens.UserName))
+			mod.error(`[DEBUG]:${msg.tokens.UserName} not in Set!`);
 
 		for (const player of players) {
 			if (player.name != msg.tokens.UserName) continue;
 			//if item already in Map, add up amounts
-			if (player.droppedItems.has(itemNames.get(parseInt(msg.tokens.ItemName[0])))) {
+			if (
+				player.droppedItems.has(itemNames.get(parseInt(msg.tokens.ItemName[0])))
+			) {
 				player.droppedItems.set(
 					itemNames.get(parseInt(msg.tokens.ItemName[0])), //get Itemname from ID in message...
-					player.droppedItems.get(itemNames.get(parseInt(msg.tokens.ItemName[0]))) + parseInt(msg.tokens.ItemAmount) //
+					player.droppedItems.get(
+						itemNames.get(parseInt(msg.tokens.ItemName[0])),
+					) + parseInt(msg.tokens.ItemAmount), //
 				);
-			} else { //otherwise add new item
+			} else {
+				//otherwise add new item
 				player.droppedItems.set(
 					itemNames.get(parseInt(msg.tokens.ItemName[0])),
-					parseInt(msg.tokens.ItemAmount)
+					parseInt(msg.tokens.ItemAmount),
 				);
 			}
-
 		}
 	}
 
 	function handleSysMsg(e) {
-		if (!config.enable) return
-		if (!config.logToChat && !config.logToFile) return
+		if (!config.enable || !(config.logMode.chat || config.logMode.file)) return;
 
 		var msg = mod.parseSystemMessage(e.message ? e.message : e.sysmsg);
-		if (msg.id == 'SMT_PARTY_LOOT_ITEM_PARTYPLAYER' || msg.id == 'SMT_LOOTED_ITEM' || msg.id == 'SMT_LOOTED_SPECIAL_ITEM') {
+		if (
+			msg.id == 'SMT_PARTY_LOOT_ITEM_PARTYPLAYER' ||
+			msg.id == 'SMT_LOOTED_ITEM' ||
+			msg.id == 'SMT_LOOTED_SPECIAL_ITEM'
+		) {
 			normaliseItemToken(msg, e);
 
 			//filter by whitelist and logging mode
 			if (!config.whitelist.includes(parseInt(msg.tokens.ItemName[0]))) return;
-			if (!config.partyLogging && msg.tokens.UserName != mod.game.me.name) return;
-			if (!config.selfLogging && msg.tokens.UserName == mod.game.me.name) return;
+			if (!config.logMode.party && msg.tokens.UserName != mod.game.me.name)
+				return;
+			if (!config.logMode.self && msg.tokens.UserName == mod.game.me.name)
+				return;
 
-			if (config.logToChat) printChatLog(msg);
-			if (config.logToFile) updateFileLog(msg);
+			if (config.logMode.chat) printChatLog(msg);
+			if (config.logMode.file) updateFileLog(msg);
 		}
 	}
 
 	function saveLog() {
-		if(players.size==0) return;
+		if (players.size == 0) return;
 
 		let logIsEmpty = true;
 		let logOutput = '';
-		
+
 		for (const player of players) {
 			if (player.droppedItems.size == 0) continue;
 			logIsEmpty = false;
-			logOutput += '\n\t' + player.name
+			logOutput += '\n\t' + player.name;
 			for (let item of player.droppedItems) {
-				logOutput += '\n\t\t> ' + item[1] + 'x ' + item[0]
+				logOutput += `\n\t\t> ${item[1]}x ${item[0]}`;
 			}
-			logOutput += '\n'
+			logOutput += '\n';
 		}
 
-		if(!logIsEmpty){
-			logOutput = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + logOutput;
+		if (!logIsEmpty) {
+			logOutput =
+				new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') +
+				logOutput;
 		}
 
 		players.clear();
 		logIsEmpty = true;
 
-		fs.appendFile('DropLog.txt', logOutput, (err) => {
+		fs.appendFile('DropLog.txt', logOutput, err => {
 			if (err) {
 				mod.error(err);
 			} else {
@@ -230,15 +289,15 @@ module.exports = function drophistory(mod) {
 	mod.hook('S_PARTY_MEMBER_LIST', 7, e => {
 		if (!config.enable) return;
 		currentParty = e.members;
-		if (!config.partyLogging) return;
+		if (!config.logMode.party) return;
 		addPartyMembers();
 	});
 
 	mod.hook('S_LOGIN', 14, e => {
-		if (config.selfLogging) AddToSetIfNew(mod.game.me.name);
-	})
+		if (config.logMode.self) AddToSetIfNew(mod.game.me.name);
+	});
 
 	this.destructor = () => {
-		if (config.logToFile) saveLog();
-	}
-}
+		if (config.logMode.file) saveLog();
+	};
+};
